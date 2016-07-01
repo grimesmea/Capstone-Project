f31837af0730aea7cc982c06a42a1978a3c51e51
mary <grimesmea@gmail.com>
2016-07-01 11:41:16 -0400

    refactor: Move Google Fit API calls out of main activity and into fragment

--- app/src/main/java/grimesmea/gmail/com/pricklefit/MainActivity.java
+++ app/src/main/java/grimesmea/gmail/com/pricklefit/MainActivity.java
@@ -1,58 +1,19 @@
 package grimesmea.gmail.com.pricklefit;
 
 import android.content.Intent;
-import android.content.IntentSender;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.MenuItem;
-import android.widget.TextView;
-
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.Scopes;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.PendingResult;
-import com.google.android.gms.common.api.Result;
-import com.google.android.gms.common.api.ResultCallback;
-import com.google.android.gms.common.api.Scope;
-import com.google.android.gms.common.api.Status;
-import com.google.android.gms.fitness.Fitness;
-import com.google.android.gms.fitness.FitnessStatusCodes;
-import com.google.android.gms.fitness.data.DataPoint;
-import com.google.android.gms.fitness.data.DataSet;
-import com.google.android.gms.fitness.data.DataSource;
-import com.google.android.gms.fitness.data.DataType;
-import com.google.android.gms.fitness.data.Field;
-import com.google.android.gms.fitness.data.Value;
-import com.google.android.gms.fitness.request.DataSourcesRequest;
-import com.google.android.gms.fitness.request.OnDataPointListener;
-import com.google.android.gms.fitness.request.SensorRequest;
-import com.google.android.gms.fitness.result.DailyTotalResult;
-import com.google.android.gms.fitness.result.DataSourcesResult;
-
-import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener,
-        OnDataPointListener {
+        implements NavigationView.OnNavigationItemSelectedListener {
 
-    private static final int REQUEST_OAUTH = 1;
-    private static final String SENSORS_AUTH_PENDING = "sensors_auth_state_pending";
-    private static final String RECORDING_AUTH_PENDING = "recording_auth_state_pending";
-    private static final String TODAY_STEP_TOTAL = "todayStepTotal";
     private final String LOG_TAG = MainActivity.class.getSimpleName();
-    private boolean sensorsAuthInProgress = false;
-    private boolean recordingAuthInProgress = false;
-    private GoogleApiClient mApiClient;
-    private int todayStepCount = 0;
-    private TextView todayStepsTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -71,163 +32,8 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
-
-        todayStepsTextView = (TextView) findViewById(R.id.today_step_count);
-        todayStepsTextView.setText("0");
-
-        if (savedInstanceState != null) {
-            sensorsAuthInProgress = savedInstanceState.getBoolean(SENSORS_AUTH_PENDING);
-            recordingAuthInProgress = savedInstanceState.getBoolean(RECORDING_AUTH_PENDING);
-
-            if (savedInstanceState.containsKey(TODAY_STEP_TOTAL)) {
-                todayStepCount = savedInstanceState.getInt(TODAY_STEP_TOTAL);
-                Log.d(LOG_TAG, "set step count using savedInstanceState");
-            }
-        }
-
-        buildSensorsApiClient();
     }
 
-    private void buildSensorsApiClient() {
-        mApiClient = new GoogleApiClient.Builder(this)
-                .addApi(Fitness.SENSORS_API)
-                .addApi(Fitness.RECORDING_API)
-                .addApi(Fitness.HISTORY_API)
-                .addScope(new Scope(Scopes.FITNESS_ACTIVITY_READ_WRITE))
-                .addConnectionCallbacks(new GoogleApiClient.ConnectionCallbacks() {
-                    @Override
-                    public void onConnected(@Nullable Bundle bundle) {
-                        // Request steps taken today so far from the Google Fit History API.
-                        getTodaysStepCount();
-
-                        // Subscribe to the Google Fit Recordings API for TYPE_STEP_COUNT_DELTA data.
-                        subscribeToRecordingApi();
-
-                        // Find and list sensors data sources using the Google Fit Sensors API for
-                        // TYPE_STEP_COUNT_DELTA data.
-                        DataSourcesRequest dataSourceRequest = new DataSourcesRequest.Builder()
-                                .setDataTypes(DataType.TYPE_STEP_COUNT_DELTA)
-                                .setDataSourceTypes(DataSource.TYPE_RAW, DataSource.TYPE_DERIVED)
-                                .build();
-
-                        ResultCallback<DataSourcesResult> dataSourcesResultCallback = new ResultCallback<DataSourcesResult>() {
-                            @Override
-                            public void onResult(DataSourcesResult dataSourcesResult) {
-                                for (DataSource dataSource : dataSourcesResult.getDataSources()) {
-                                    Log.d(LOG_TAG, "Data source: " + dataSource.getDevice());
-
-                                    if (DataType.TYPE_STEP_COUNT_DELTA.equals(dataSource.getDataType())) {
-                                        registerFitnessDataListener(dataSource, DataType.TYPE_STEP_COUNT_DELTA);
-                                    }
-                                }
-                            }
-                        };
-
-                        Fitness.SensorsApi.findDataSources(mApiClient, dataSourceRequest)
-                                .setResultCallback(dataSourcesResultCallback);
-                    }
-
-                    @Override
-                    public void onConnectionSuspended(int i) {
-
-                    }
-                })
-                .enableAutoManage(this, 0, new GoogleApiClient.OnConnectionFailedListener() {
-                    @Override
-                    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-                        if (!sensorsAuthInProgress) {
-                            try {
-                                sensorsAuthInProgress = true;
-                                connectionResult.startResolutionForResult(MainActivity.this, REQUEST_OAUTH);
-                            } catch (IntentSender.SendIntentException e) {
-
-                            }
-                        } else {
-                            Log.e(LOG_TAG, "sensorsAuthInProgress");
-                        }
-                    }
-                })
-                .build();
-    }
-
-    private void getTodaysStepCount() {
-        PendingResult<DailyTotalResult> stepsResult = Fitness.HistoryApi
-                .readDailyTotal(mApiClient, DataType.TYPE_STEP_COUNT_DELTA);
-        stepsResult.setResultCallback(new ResultCallback() {
-            @Override
-            public void onResult(Result result) {
-                DailyTotalResult dailyTotalResult = (DailyTotalResult) result;
-                if (result.getStatus().isSuccess()) {
-                    DataSet totalSet = dailyTotalResult.getTotal();
-                    todayStepCount = totalSet.isEmpty()
-                            ? 0
-                            : totalSet.getDataPoints().get(0).getValue(Field.FIELD_STEPS).asInt();
-                    Log.d(LOG_TAG, "Daily step total retrieved from history API. Today's steps = " + Integer.toString(todayStepCount));
-                    updateStepCountTextView(todayStepCount);
-                }
-            }
-        });
-    }
-
-    public void subscribeToRecordingApi() {
-        // To create a subscription, invoke the Recording API. As soon as the subscription is
-        // active, fitness data will start recording.
-        Fitness.RecordingApi.subscribe(mApiClient, DataType.TYPE_STEP_COUNT_DELTA)
-                .setResultCallback(new ResultCallback<Status>() {
-                    @Override
-                    public void onResult(Status status) {
-                        if (status.isSuccess()) {
-                            if (status.getStatusCode()
-                                    == FitnessStatusCodes.SUCCESS_ALREADY_SUBSCRIBED) {
-                                Log.i(LOG_TAG, "Existing subscription for activity detected.");
-                            } else {
-                                Log.i(LOG_TAG, "Successfully subscribed!");
-                            }
-                        } else {
-                            Log.i(LOG_TAG, "There was a problem subscribing.");
-                        }
-                    }
-                });
-    }
-
-    private void registerFitnessDataListener(DataSource dataSource, DataType dataType) {
-        SensorRequest request = new SensorRequest.Builder()
-                .setDataSource(dataSource)
-                .setDataType(dataType)
-                .setSamplingRate(3, TimeUnit.SECONDS)
-                .build();
-
-        Fitness.SensorsApi.add(mApiClient, request, this)
-                .setResultCallback(new ResultCallback<Status>() {
-                    @Override
-                    public void onResult(Status status) {
-                        if (status.isSuccess()) {
-                            Log.e(LOG_TAG, "SensorApi successfully added");
-                        }
-                    }
-                });
-    }
-
-    @Override
-    public void onDataPoint(DataPoint dataPoint) {
-        for (final Field field : dataPoint.getDataType().getFields()) {
-            final Value value = dataPoint.getValue(field);
-            runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    Log.d(LOG_TAG, "DELTA STEPS " + Integer.toString(value.asInt()));
-                    if (value.asInt() > 0) {
-                        todayStepCount += value.asInt();
-                        updateStepCountTextView(todayStepCount);
-                    }
-                }
-            });
-        }
-    }
-
-    private void updateStepCountTextView(int currentStepCount) {
-        todayStepsTextView.setText(String.format("%,d", currentStepCount));
-    }
 
     @Override
     public void onBackPressed() {
@@ -239,7 +45,6 @@ public class MainActivity extends AppCompatActivity
         }
     }
 
-    @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
@@ -259,13 +64,5 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
-    }
-
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-        outState.putBoolean(SENSORS_AUTH_PENDING, sensorsAuthInProgress);
-        outState.putBoolean(RECORDING_AUTH_PENDING, recordingAuthInProgress);
-        outState.putInt(TODAY_STEP_TOTAL, todayStepCount);
     }
 }
--- app/src/main/java/grimesmea/gmail/com/pricklefit/TodayStepsFragment.java
+++ app/src/main/java/grimesmea/gmail/com/pricklefit/TodayStepsFragment.java
@@ -1,12 +1,17 @@
 package grimesmea.gmail.com.pricklefit;
 
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.IntentSender;
 import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.CursorLoader;
@@ -18,13 +23,37 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.Scopes;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.Result;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Scope;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.fitness.Fitness;
+import com.google.android.gms.fitness.FitnessStatusCodes;
+import com.google.android.gms.fitness.data.DataPoint;
+import com.google.android.gms.fitness.data.DataSet;
+import com.google.android.gms.fitness.data.DataSource;
+import com.google.android.gms.fitness.data.DataType;
+import com.google.android.gms.fitness.data.Field;
+import com.google.android.gms.fitness.data.Value;
+import com.google.android.gms.fitness.request.DataSourcesRequest;
+import com.google.android.gms.fitness.request.OnDataPointListener;
+import com.google.android.gms.fitness.request.SensorRequest;
+import com.google.android.gms.fitness.result.DailyTotalResult;
+import com.google.android.gms.fitness.result.DataSourcesResult;
+
+import java.util.concurrent.TimeUnit;
+
 import grimesmea.gmail.com.pricklefit.data.HedgehogContract.HedgehogsEntry;
 
 
 /**
  * Displays daily step count and goal.
  */
-public class TodayStepsFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
+public class TodayStepsFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor>, OnDataPointListener {
 
     static final int COL_HEDGEHOG_ID = 0;
     static final int COL_HEDGEHOG_NAME = 1;
@@ -35,10 +64,22 @@ public class TodayStepsFragment extends Fragment implements LoaderManager.Loader
     static final int COL_HEDGEHOG_FITNESS_LEVEL = 6;
     static final int COL_HEDGEHOG_UNLOCK_STATUS = 7;
     static final int COL_HEDGEHOG_SELECTED_STATUS = 8;
+
     private static final int SELECTED_HEDGEHOG_LOADER = 100;
+    private static final int REQUEST_OAUTH = 1;
+    private static final String SENSORS_AUTH_PENDING = "sensors_auth_state_pending";
+    private static final String RECORDING_AUTH_PENDING = "recording_auth_state_pending";
+    private static final String TODAY_STEP_TOTAL = "todayStepTotal";
     private final String LOG_TAG = TodayStepsFragment.class.getSimpleName();
-    int dailyStepGoal = 0;
+    private boolean sensorsAuthInProgress = false;
+    private boolean recordingAuthInProgress = false;
+    private GoogleApiClient mApiClient;
+
+    private Activity activity;
+    private int todayStepCount = 0;
+    private int dailyStepGoal = 0;
     private ImageView hedgehogImageView;
+    private TextView todayStepsTextView;
     private TextView dailyStepGoalTextView;
     private Hedgehog selectedHedgehog;
     private Drawable hedgehogDrawable;
@@ -60,11 +101,14 @@ public class TodayStepsFragment extends Fragment implements LoaderManager.Loader
         ).moveToNext()) {
             fetchHedgehogs();
         }
-    }
 
-    private void fetchHedgehogs() {
-        FetchHedgehogsDataTask hedgehogsDataTasks = new FetchHedgehogsDataTask(getActivity());
-        hedgehogsDataTasks.execute();
+        if (savedInstanceState != null) {
+            sensorsAuthInProgress = savedInstanceState.getBoolean(SENSORS_AUTH_PENDING);
+            recordingAuthInProgress = savedInstanceState.getBoolean(RECORDING_AUTH_PENDING);
+            todayStepCount = savedInstanceState.getInt(TODAY_STEP_TOTAL);
+        }
+
+        buildSensorsApiClient();
     }
 
     @Override
@@ -72,7 +116,12 @@ public class TodayStepsFragment extends Fragment implements LoaderManager.Loader
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_todays_steps, container, false);
         hedgehogImageView = (ImageView) rootView.findViewById(R.id.hedgehog_image);
+        todayStepsTextView = (TextView) rootView.findViewById(R.id.today_step_count);
         dailyStepGoalTextView = (TextView) rootView.findViewById(R.id.today_step_goal);
+
+        if (todayStepCount > 0) {
+            updateStepCountTextView(todayStepCount);
+        }
 
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getContext());
         String dailyStepGoalStr = prefs.getString(getContext().getString(R.string.pref_step_goal_key),
@@ -89,6 +138,167 @@ public class TodayStepsFragment extends Fragment implements LoaderManager.Loader
         getLoaderManager().initLoader(SELECTED_HEDGEHOG_LOADER, null, this);
 
         return rootView;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof Activity) {
+            activity = (Activity) context;
+        }
+
+    }
+
+    private void fetchHedgehogs() {
+        FetchHedgehogsDataTask hedgehogsDataTasks = new FetchHedgehogsDataTask(getActivity());
+        hedgehogsDataTasks.execute();
+    }
+
+    private void buildSensorsApiClient() {
+        mApiClient = new GoogleApiClient.Builder(getActivity())
+                .addApi(Fitness.SENSORS_API)
+                .addApi(Fitness.RECORDING_API)
+                .addApi(Fitness.HISTORY_API)
+                .addScope(new Scope(Scopes.FITNESS_ACTIVITY_READ_WRITE))
+                .addConnectionCallbacks(new GoogleApiClient.ConnectionCallbacks() {
+                    @Override
+                    public void onConnected(@Nullable Bundle bundle) {
+                        // Request steps taken today so far from the Google Fit History API.
+                        getTodaysStepCount();
+
+                        // Subscribe to the Google Fit Recordings API for TYPE_STEP_COUNT_DELTA data.
+                        subscribeToRecordingApi();
+
+                        // Find and list sensors data sources using the Google Fit Sensors API for
+                        // TYPE_STEP_COUNT_DELTA data.
+                        DataSourcesRequest dataSourceRequest = new DataSourcesRequest.Builder()
+                                .setDataTypes(DataType.TYPE_STEP_COUNT_DELTA)
+                                .setDataSourceTypes(DataSource.TYPE_RAW, DataSource.TYPE_DERIVED)
+                                .build();
+
+                        ResultCallback<DataSourcesResult> dataSourcesResultCallback = new ResultCallback<DataSourcesResult>() {
+                            @Override
+                            public void onResult(DataSourcesResult dataSourcesResult) {
+                                for (DataSource dataSource : dataSourcesResult.getDataSources()) {
+                                    Log.d(LOG_TAG, "Data source: " + dataSource.getDevice());
+
+                                    if (DataType.TYPE_STEP_COUNT_DELTA.equals(dataSource.getDataType())) {
+                                        registerFitnessDataListener(dataSource, DataType.TYPE_STEP_COUNT_DELTA);
+                                    }
+                                }
+                            }
+                        };
+
+                        Fitness.SensorsApi.findDataSources(mApiClient, dataSourceRequest)
+                                .setResultCallback(dataSourcesResultCallback);
+                    }
+
+                    @Override
+                    public void onConnectionSuspended(int i) {
+
+                    }
+                })
+                .enableAutoManage(getActivity(), 0, new GoogleApiClient.OnConnectionFailedListener() {
+                    @Override
+                    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+                        if (!sensorsAuthInProgress) {
+                            try {
+                                sensorsAuthInProgress = true;
+                                connectionResult.startResolutionForResult(getActivity(), REQUEST_OAUTH);
+                            } catch (IntentSender.SendIntentException e) {
+
+                            }
+                        } else {
+                            Log.e(LOG_TAG, "sensorsAuthInProgress");
+                        }
+                    }
+                })
+                .build();
+    }
+
+    private void getTodaysStepCount() {
+        PendingResult<DailyTotalResult> stepsResult = Fitness.HistoryApi
+                .readDailyTotal(mApiClient, DataType.TYPE_STEP_COUNT_DELTA);
+        stepsResult.setResultCallback(new ResultCallback() {
+            @Override
+            public void onResult(Result result) {
+                DailyTotalResult dailyTotalResult = (DailyTotalResult) result;
+                if (result.getStatus().isSuccess()) {
+                    DataSet totalSet = dailyTotalResult.getTotal();
+                    todayStepCount = totalSet.isEmpty()
+                            ? 0
+                            : totalSet.getDataPoints().get(0).getValue(Field.FIELD_STEPS).asInt();
+                    Log.d(LOG_TAG, "Daily step total retrieved from history API. Today's steps = " + Integer.toString(todayStepCount));
+                    updateStepCountTextView(todayStepCount);
+                }
+            }
+        });
+    }
+
+    public void subscribeToRecordingApi() {
+        // To create a subscription, invoke the Recording API. As soon as the subscription is
+        // active, fitness data will start recording.
+        Fitness.RecordingApi.subscribe(mApiClient, DataType.TYPE_STEP_COUNT_DELTA)
+                .setResultCallback(new ResultCallback<Status>() {
+                    @Override
+                    public void onResult(Status status) {
+                        if (status.isSuccess()) {
+                            if (status.getStatusCode()
+                                    == FitnessStatusCodes.SUCCESS_ALREADY_SUBSCRIBED) {
+                                Log.i(LOG_TAG, "Existing subscription for activity detected.");
+                            } else {
+                                Log.i(LOG_TAG, "Successfully subscribed!");
+                            }
+                        } else {
+                            Log.i(LOG_TAG, "There was a problem subscribing.");
+                        }
+                    }
+                });
+    }
+
+    private void registerFitnessDataListener(DataSource dataSource, DataType dataType) {
+        SensorRequest request = new SensorRequest.Builder()
+                .setDataSource(dataSource)
+                .setDataType(dataType)
+                .setSamplingRate(3, TimeUnit.SECONDS)
+                .build();
+
+        Fitness.SensorsApi.add(mApiClient, request, this)
+                .setResultCallback(new ResultCallback<Status>() {
+                    @Override
+                    public void onResult(Status status) {
+                        if (status.isSuccess()) {
+                            Log.e(LOG_TAG, "SensorApi successfully added");
+                        }
+                    }
+                });
+    }
+
+    @Override
+    public void onDataPoint(DataPoint dataPoint) {
+        for (final Field field : dataPoint.getDataType().getFields()) {
+            final Value value = dataPoint.getValue(field);
+
+            if (activity == null) {
+                Log.e(LOG_TAG, "activity == null");
+                return;
+            }
+
+            activity.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    Log.d(LOG_TAG, "DELTA STEPS " + Integer.toString(value.asInt()));
+                    if (value.asInt() > 0) {
+                        todayStepCount += value.asInt();
+                        updateStepCountTextView(todayStepCount);
+                    }
+                }
+            });
+        }
+    }
+
+    private void updateStepCountTextView(int currentStepCount) {
+        todayStepsTextView.setText(String.format("%,d", currentStepCount));
     }
 
     @Override
@@ -127,5 +337,13 @@ public class TodayStepsFragment extends Fragment implements LoaderManager.Loader
         hedgehogImageView.setImageDrawable(null);
         selectedHedgehog = null;
         hedgehogDrawable = null;
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putBoolean(SENSORS_AUTH_PENDING, sensorsAuthInProgress);
+        outState.putBoolean(RECORDING_AUTH_PENDING, recordingAuthInProgress);
+        outState.putInt(TODAY_STEP_TOTAL, todayStepCount);
     }
 }
